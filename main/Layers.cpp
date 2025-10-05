b98207a3ebec9928a2d925fb2fdcaefd51e58c96
Julius Kirsch <Julius@skirsch.de>
2025-10-05 16:28:10 +0200

    fix: softmax activation handled wrong in training

--- main/Layers.cpp
+++ main/Layers.cpp
@@ -2,93 +2,107 @@
 #include "NeuralNetwork.h"
 #include "Arduino.h"
 
-
-float Activation(float value, ActivationKind activationKind) {
-    switch (activationKind) {
-        case ActivationKind::Sigmoid:
-            return 1.0 / (1.0 + exp(-value));
-        case ActivationKind::Relu:
-            return value > 0 ? value : 0;
-        case ActivationKind::TanH:
-            return tanh(value);
-        case ActivationKind::LeakyRelu:
-            return value > 0 ? value : 0.01 * value;
-        default: 
-          Serial.println("Invalid Activation Index");
-          return value;
-    }
+float Activation(float value, ActivationKind activationKind)
+{
+  switch (activationKind)
+  {
+  case ActivationKind::Sigmoid:
+    return 1.0 / (1.0 + exp(-value));
+  case ActivationKind::Relu:
+    return value > 0 ? value : 0;
+  case ActivationKind::TanH:
+    return tanh(value);
+  case ActivationKind::LeakyRelu:
+    return value > 0 ? value : 0.01 * value;
+  default:
+    Serial.println("Invalid Activation Index");
+    return value;
+  }
 }
 
-float ActivationDeriv(float value, ActivationKind activationKind) {
-    switch (activationKind) {
-        case ActivationKind::Sigmoid:
-            return value * (1.0 - value);
-        case ActivationKind::Relu:
-            return value > 0 ? 1.0 : 0.0;
-        case ActivationKind::TanH:
-            return 1.0 - value * value;
-        case ActivationKind::LeakyRelu:
-            return value > 0 ? 1.0 : 0.010;
-        default:
-          Serial.println("Invalid Activation Index");
-          return value;
-    }
+float ActivationDeriv(float value, ActivationKind activationKind)
+{
+  switch (activationKind)
+  {
+  case ActivationKind::Sigmoid:
+    return value * (1.0 - value);
+  case ActivationKind::Relu:
+    return value > 0 ? 1.0 : 0.0;
+  case ActivationKind::TanH:
+    return 1.0 - value * value;
+  case ActivationKind::LeakyRelu:
+    return value > 0 ? 1.0 : 0.010;
+  default:
+    Serial.println("Invalid Activation Index");
+    return value;
+  }
 }
 
-void ActivationSoftmax(float* values, int size) {
+void ActivationSoftmax(float *values, int size)
+{
   // For numerical stability, subtract max value first
   float maxVal = values[0];
-  for (int i = 1; i < size; i++) {
-    if (values[i] > maxVal) maxVal = values[i];
+  for (int i = 1; i < size; i++)
+  {
+    if (values[i] > maxVal)
+      maxVal = values[i];
   }
 
   // Exponentiate shifted values and sum
   float sum = 0.0f;
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++)
+  {
     values[i] = exp(values[i] - maxVal);
     sum += values[i];
   }
 
   // Normalize
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size; i++)
+  {
     values[i] /= sum;
   }
 }
 
-
-void FillRandom(float* array, int size) {
-  for (int i = 0; i < size; i++) {
+void FillRandom(float *array, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     float rand = (random(0, 32768) / 32768.0) * 2 - 1;
     array[i] = rand;
   }
 }
 
-void XavierInitializeWeights(float * weights, int size, int fanIn, int fanOut){
+void XavierInitializeWeights(float *weights, int size, int fanIn, int fanOut)
+{
   float limit = sqrt(6.0 / (fanIn + fanOut));
-  for(int i = 0; i<size; i++){
+  for (int i = 0; i < size; i++)
+  {
     weights[i] = random(0, 32768) / 32768.0 * (limit * 2) - limit;
   }
 }
 
-///BASE LAYER
+/// BASE LAYER
 BaseLayer::BaseLayer()
-  : Biases(nullptr), NeuronValues(nullptr), Errors(nullptr), Weights(nullptr),
-    Size(0), PreviousLayer(nullptr), NextLayer(nullptr) {}
-BaseLayer::~BaseLayer() {
+    : Biases(nullptr), NeuronValues(nullptr), Errors(nullptr), Weights(nullptr),
+      Size(0), PreviousLayer(nullptr), NextLayer(nullptr) {}
+BaseLayer::~BaseLayer()
+{
   delete[] Biases;
   delete[] NeuronValues;
   delete[] Errors;
   delete[] Weights;
 }
 
-///DENSE LAYER
+/// DENSE LAYER
 DenseLayer::DenseLayer(int size, ActivationKind activationKind)
-  : BaseLayer() {
+    : BaseLayer()
+{
   this->Size = size;
   this->activationKind = activationKind;
 }
 DenseLayer::~DenseLayer() {}
-void DenseLayer::InitLayer(NeuralNetwork * nn, int size, BaseLayer* previous, BaseLayer* next) {
+void DenseLayer::InitLayer(NeuralNetwork *nn, int size, BaseLayer *previous, BaseLayer *next)
+{
   this->Size = size;
   this->Biases = new float[size];
   this->NeuronValues = new float[size];
@@ -101,22 +115,28 @@ void DenseLayer::InitLayer(NeuralNetwork * nn, int size, BaseLayer* previous, Ba
 
   XavierInitializeWeights(this->Weights, size * previous->Size, nn->allLayer[0]->Size, nn->allLayer[nn->totalLayers - 1]->Size);
 }
-void DenseLayer::FeedForward() {
-  for (int idx = 0; idx < this->Size; idx++) {
+void DenseLayer::FeedForward()
+{
+  for (int idx = 0; idx < this->Size; idx++)
+  {
     float sum = 0.0f;
     int index = idx * this->PreviousLayer->Size;
-    for (int j = 0; j < this->PreviousLayer->Size; j++) {
+    for (int j = 0; j < this->PreviousLayer->Size; j++)
+    {
       sum += this->PreviousLayer->NeuronValues[j] * this->Weights[index + j];
     }
     this->NeuronValues[idx] = Activation(sum + this->Biases[idx], this->activationKind);
   }
 }
-void DenseLayer::Train(const float* desiredValues, float learningRate) {
-  for (int idx = 0; idx < this->Size; idx++) {
+void DenseLayer::Train(const float *desiredValues, float learningRate)
+{
+  for (int idx = 0; idx < this->Size; idx++)
+  {
     float err = 0.0f;
     int index = idx * this->PreviousLayer->Size;
 
-    for (int j = 0; j < this->NextLayer->Size; j++) {
+    for (int j = 0; j < this->NextLayer->Size; j++)
+    {
       err += (this->NextLayer->Errors[j] * this->NextLayer->Weights[j * this->Size + idx]);
     }
     float error = err * ActivationDeriv(this->NeuronValues[idx], this->activationKind);
@@ -124,7 +144,8 @@ void DenseLayer::Train(const float* desiredValues, float learningRate) {
 
     error *= learningRate;
 
-    for (int j = 0; j < this->PreviousLayer->Size; j++) {
+    for (int j = 0; j < this->PreviousLayer->Size; j++)
+    {
       this->Weights[index + j] += error * this->PreviousLayer->NeuronValues[j];
     }
 
@@ -132,31 +153,34 @@ void DenseLayer::Train(const float* desiredValues, float learningRate) {
   }
 }
 
-//INPUT LAYER
+// INPUT LAYER
 InputLayer::InputLayer(int size)
-  : BaseLayer() {
+    : BaseLayer()
+{
   this->Size = size;
   this->activationKind = activationKind;
 }
 InputLayer::~InputLayer() {}
-void InputLayer::InitLayer(NeuralNetwork * nn, int size, BaseLayer* previous, BaseLayer* next) {
+void InputLayer::InitLayer(NeuralNetwork *nn, int size, BaseLayer *previous, BaseLayer *next)
+{
   this->Size = size;
   this->NeuronValues = new float[size];
   this->PreviousLayer = previous;
   this->NextLayer = next;
 }
 void InputLayer::FeedForward() {}
-void InputLayer::Train(const float* desiredValues, float learningRate) {}
+void InputLayer::Train(const float *desiredValues, float learningRate) {}
 
-
-//OUTPUT LAYER
+// OUTPUT LAYER
 OutputLayer::OutputLayer(int size, ActivationKind activationKind)
-  : BaseLayer() {
+    : BaseLayer()
+{
   this->Size = size;
   this->activationKind = activationKind;
 }
 OutputLayer::~OutputLayer() {}
-void OutputLayer::InitLayer(NeuralNetwork* nn, int size, BaseLayer* previous, BaseLayer* next) {
+void OutputLayer::InitLayer(NeuralNetwork *nn, int size, BaseLayer *previous, BaseLayer *next)
+{
   this->Size = size;
   this->Biases = new float[size];
   this->NeuronValues = new float[size];
@@ -170,61 +194,75 @@ void OutputLayer::InitLayer(NeuralNetwork* nn, int size, BaseLayer* previous, Ba
   FillRandom(this->Biases, size);
 }
 
-void OutputLayer::FeedForward_Softmax(){
-   for (int idx = 0; idx < this->Size; idx++) {
-      float sum = 0.0f;
-      int weightIndex = idx * this->PreviousLayer->Size;
-      for (int j = 0; j < this->PreviousLayer->Size; j++) {
-        sum += this->PreviousLayer->NeuronValues[j] * this->Weights[weightIndex + j];
-      }
-
-      this->NeuronValues[idx] = sum + this->Biases[idx];
+void OutputLayer::FeedForward_Softmax()
+{
+  for (int idx = 0; idx < this->Size; idx++)
+  {
+    float sum = 0.0f;
+    int weightIndex = idx * this->PreviousLayer->Size;
+    for (int j = 0; j < this->PreviousLayer->Size; j++)
+    {
+      sum += this->PreviousLayer->NeuronValues[j] * this->Weights[weightIndex + j];
     }
 
-    ActivationSoftmax(this->NeuronValues, this->Size);
+    this->NeuronValues[idx] = sum + this->Biases[idx];
+  }
+
+  ActivationSoftmax(this->NeuronValues, this->Size);
 }
 
-void OutputLayer::FeedForward() {
-  //handle softmax activation:
-  if (this->activationKind == Softmax) {
+void OutputLayer::FeedForward()
+{
+  // handle softmax activation:
+  if (this->activationKind == Softmax)
+  {
     FeedForward_Softmax();
     return;
   }
-  
-  //handle other cases:
-  for (int idx = 0; idx < this->Size; idx++) {
+
+  // handle other cases:
+  for (int idx = 0; idx < this->Size; idx++)
+  {
     float sum = 0.0f;
     int weightIndex = idx * this->PreviousLayer->Size;
-    for (int j = 0; j < this->PreviousLayer->Size; j++) {
+    for (int j = 0; j < this->PreviousLayer->Size; j++)
+    {
       sum += this->PreviousLayer->NeuronValues[j] * this->Weights[weightIndex + j];
     }
     this->NeuronValues[idx] = Activation(sum + this->Biases[idx], this->activationKind);
   }
 }
 
-void OutputLayer::Train(const float* desiredValues, float learningRate) {
-  for (int idx = 0; idx < this->Size; idx++) {
+void OutputLayer::Train(const float *desiredValues, float learningRate)
+{
+  // error
+  for (int idx = 0; idx < this->Size; idx++)
+  {
     this->Errors[idx] = desiredValues[idx] - this->NeuronValues[idx];
   }
 
-  for (int idx = 0; idx < this->Size; idx++) {
-    float derivNeuronVal = learningRate * this->Errors[idx] * ActivationDeriv(this->NeuronValues[idx], this->activationKind);
-
-    float grad; 
-    if (this->activationKind == Sigmoid || this->activationKind == Softmax) {
-      //Crossentropy + sigmoid or softmax
-      grad = learningRate * this->Errors[idx];
-    } else {
-      // MSE + generic activation
-      grad = learningRate * this->Errors[idx] *
-             ActivationDeriv(this->NeuronValues[idx], this->activationKind);
+  // weights + bias
+  for (int idx = 0; idx < this->Size; idx++)
+  {
+    float gradZ;
+    if (this->activationKind == Sigmoid || this->activationKind == Softmax)
+    {
+      // Cross-entropy with sigmoid/softmax
+      gradZ = this->Errors[idx];
+    }
+    else
+    {
+      // MSE or other loss
+      gradZ = this->Errors[idx] * ActivationDeriv(this->NeuronValues[idx], this->activationKind);
     }
 
     int weightIndex = idx * this->PreviousLayer->Size;
-    for (int j = 0; j < this->PreviousLayer->Size; j++) {
-      this->Weights[weightIndex + j] += derivNeuronVal * this->PreviousLayer->NeuronValues[j];
+
+    for (int j = 0; j < this->PreviousLayer->Size; j++)
+    {
+      this->Weights[weightIndex + j] += learningRate * gradZ * this->PreviousLayer->NeuronValues[j];
     }
 
-    this->Biases[idx] += grad;
+    this->Biases[idx] += learningRate * gradZ;
   }
 }
